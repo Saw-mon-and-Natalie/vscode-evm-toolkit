43bb4bf001db23f670e7e6858974c508205d9fbd
Saw-mon-and-Natalie <samangharib50233@gmail.com>
2022-04-09 11:26:43 -0700

    cleaned extensions.js for a more fresh setup

--- src/extension.js
+++ src/extension.js
@@ -1,8 +1,8 @@
-'use strict';
-/** 
+"use strict";
+/**
  * @author github.com/Saw-mon-and-Natalie
  * @license MIT
- * 
+ *
  * */
 
 /** imports */
@@ -13,20 +13,23 @@ const settings = require("./settings");
 var activeEditor;
 
 function onActivate(context) {
-    const active = vscode.window.activeTextEditor;
-    activeEditor = active;
+  const active = vscode.window.activeTextEditor;
+  activeEditor = active;
 
-    registerDocType(settings.LANGUAGE_ID);
+  registerDocType(settings.LANGUAGE_ID);
 
-    function registerDocType(type) {
-        context.subscriptions.push(
-            vscode.languages.reg
-        );
-        vscode.window.onDidChangeActiveTextEditor(editor => {
-            activeEditor = editor;
-            if (editor) {}
-        }, null, context.subscriptions);
-    }
+  function registerDocType(type) {
+    context.subscriptions.push(vscode.languages.reg);
+    vscode.window.onDidChangeActiveTextEditor(
+      (editor) => {
+        activeEditor = editor;
+        if (editor) {
+        }
+      },
+      null,
+      context.subscriptions
+    );
+  }
 }
 
 /* exports */
